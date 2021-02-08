83d6390a286aaabe238e4bdf4817f638be3568de
Tim <tim.uniewski@gmail.com>
2021-02-08 19:36:31 +0100

    Working Example

---
+++ PasswordWorks/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasswordWorks
+{
+    public class ViewModel : ObservableObject
+    {
+        private string _Password;
+        public string Password
+        {
+            get { return _Password; }
+            set { SetProperty(ref _Password, value); }
+        }
+
+    }
+}
